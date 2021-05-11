2c2144fdc6edc56743003c4cd5cfb03fd9ec6861
David Vargas <dvargas92495@gmail.com>
2021-05-11 11:39:59 -0600

    Standing up resources for otter backend

---
+++ aws.tf
@@ -0,0 +1,163 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "VargasArts"
+    workspaces {
+      prefix = "roamjs-otter"
+    }
+  }
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "4.2.0"
+    }
+  }
+}
+
+variable "aws_access_token" {
+  type = string
+}
+
+variable "aws_secret_token" {
+  type = string
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = var.aws_access_token
+    secret_key = var.aws_secret_token
+}
+
+# lambda resource requires either filename or s3... wow
+data "archive_file" "dummy" {
+  type        = "zip"
+  output_path = "./dummy.zip"
+
+  source {
+    content   = "// TODO IMPLEMENT"
+    filename  = "dummy.js"
+  }
+}
+
+data "aws_iam_role" "roamjs_lambda_role" {
+  name = "roam-js-extensions-lambda-execution"
+}
+
+data "aws_api_gateway_rest_api" "rest_api" {
+  name = "roamjs-extensions"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = data.aws_api_gateway_rest_api.id
+  parent_id   = data.aws_api_gateway_rest_api.root_resource_id
+  path_part   = "otter"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  function_name = "RoamJS_otter"
+  role          = data.aws_iam_role.roamjs_lambda_role.arn
+  handler       = "otter.handler"
+  filename      = data.archive_file.dummy.output_path
+  runtime       = "nodejs12.x"
+  publish       = false
+  timeout       = 10
+
+  tags = {
+    Application = "Roam JS Extensions"
+  }
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = data.aws_api_gateway_rest_api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = data.aws_api_gateway_rest_api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_function.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${data.aws_api_gateway_rest_api.execution_arn}/*/*/*"
+}
+
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = data.aws_api_gateway_rest_api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "mock" {
+  rest_api_id          = data.aws_api_gateway_rest_api.id
+  resource_id          = aws_api_gateway_resource.resource.id
+  http_method          = aws_api_gateway_method.options.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
+
+  request_templates = {
+    "application/json" = jsonencode(
+        {
+            statusCode = 200
+        }
+    )
+  }
+}
+
+resource "aws_api_gateway_method_response" "mock" {
+  rest_api_id = data.aws_api_gateway_rest_api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+  
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = true,
+    "method.response.header.Access-Control-Allow-Methods"     = true,
+    "method.response.header.Access-Control-Allow-Origin"      = true,
+    "method.response.header.Access-Control-Allow-Credentials" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "mock" {
+  rest_api_id = data.aws_api_gateway_rest_api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = aws_api_gateway_method_response.mock.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = "'Authorization, Content-Type'",
+    "method.response.header.Access-Control-Allow-Methods"     = "'GET,DELETE,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"      = "'*'",
+    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
+  }
+}
+
+provider "github" {
+    owner = "dvargas92495"
+}
+
+resource "github_actions_secret" "deploy_aws_access_key" {
+  repository       = "roamjs-otter"
+  secret_name      = "DEPLOY_AWS_ACCESS_KEY"
+  plaintext_value  = var.aws_access_token
+}
+
+resource "github_actions_secret" "deploy_aws_access_secret" {
+  repository       = "roamjs-otter"
+  secret_name      = "DEPLOY_AWS_ACCESS_SECRET"
+  plaintext_value  = var.aws_secret_token
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
+import { createConfigObserver } from "roamjs-components";
+import { render } from "./components/ImportOtterDialog";
+
 const CONFIG = `roam/js/otter`;
+
+createConfigObserver({
+  title: CONFIG,
+  config: {
+    tabs: [
+      {
+        fields: [
+          {
+            type: "text",
+            title: "email",
+            description: "The email tied to your Otter account",
+          },
+          {
+            type: "text",
+            title: "password",
+            description: "The password needed to access your Otter account",
+          },
+        ],
+        id: "home",
+      },
+    ],
+  },
+});
+
+window.roamAlphaAPI.ui.commandPalette.addCommand({
+  label: "Import Otter",
+  callback: () => render({}),
+});
