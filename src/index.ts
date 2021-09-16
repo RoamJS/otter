28db4d3dde23e50cac300aa99cefddc33ddd9a89
David Vargas <dvargas92495@gmail.com>
2021-09-16 18:19:14 -0400

    wrap in run extension

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,47 @@
-import { addBlockCommand } from "roam-client";
+import { addBlockCommand, runExtension } from "roam-client";
 import { createConfigObserver } from "roamjs-components";
 import { render } from "./components/ImportOtterDialog";
 
 const CONFIG = `roam/js/otter`;
 
-createConfigObserver({
-  title: CONFIG,
-  config: {
-    tabs: [
-      {
-        fields: [
-          {
-            type: "text",
-            title: "email",
-            description: "The email tied to your Otter account",
-          },
-          {
-            type: "text",
-            title: "password",
-            description: "The password needed to access your Otter account",
-          },
-          {
-            type: "text",
-            title: "label",
-            description: "The format labels use on import",
-          },
-          {
-            type: "text",
-            title: "template",
-            description: "The format each Otter note/transcript uses on import",
-          },
-        ],
-        id: "home",
-      },
-    ],
-    versioning: true,
-  },
-});
+runExtension("otter", () => {
+  createConfigObserver({
+    title: CONFIG,
+    config: {
+      tabs: [
+        {
+          fields: [
+            {
+              type: "text",
+              title: "email",
+              description: "The email tied to your Otter account",
+            },
+            {
+              type: "text",
+              title: "password",
+              description: "The password needed to access your Otter account",
+            },
+            {
+              type: "text",
+              title: "label",
+              description: "The format labels use on import",
+            },
+            {
+              type: "text",
+              title: "template",
+              description:
+                "The format each Otter note/transcript uses on import",
+            },
+          ],
+          id: "home",
+        },
+      ],
+      versioning: true,
+    },
+  });
 
-addBlockCommand({
-  label: "Import Otter",
-  callback: (blockUid) => render({ blockUid }),
+  addBlockCommand({
+    label: "Import Otter",
+    callback: (blockUid) => render({ blockUid }),
+  });
 });
